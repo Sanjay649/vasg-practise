b3760d0f5eb853ce44635b505494e8f3432788e6
Sanjay-244 <svadla@memphis.edu>
2026-04-14 16:51:52 -0400

    Add student data and JSON export in lesson 2.py

---
+++ week1python/lesson 2.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+import json
+student = {
+    "id": 1,
+    "name": "Ravi",
+    "age": 15
+}
+
+with open("student.json", "w") as file:
+    json.dump(student, file, indent=4)
+
+
+
+git remote add origin https://github.com/yourusername/yourrepo.git
